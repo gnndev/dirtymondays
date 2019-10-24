45420c5719286733b343ebf31dae7cbaac3995eb
Giovanni <giovanni.puccio@gmail.com>
2019-10-24 16:04:34 +0200

    fix carousel

--- parts/blocks/content-carousel.php
+++ parts/blocks/content-carousel.php
@@ -9,8 +9,11 @@
 $align_class = $block['align'] ? 'align' . $block['align'] : '';
  ?>
 
- <div class="grid-container block-carousel <?php echo $align_class; ?>">
- <div class="slick-carousel">
+ <div class=" block-carousel <?php echo $align_class; ?>">
+ <div class="grid-container">
+    <div class="block-header"><div class="block-title"><p ><?php the_field('carousel_block_title'); ?></p></div> <div class="block-link"><a href="<?php echo (wp_is_mobile()) ? get_field('carousel_block_url_mobile') : get_field('carousel_block_url'); ?>" target="_blank"><?php the_field('carousel_block_link'); ?> &rarr;</a></div> </div>
+ 
+    <div class="slick-carousel">
   
 
 
@@ -23,9 +26,9 @@ $align_class = $block['align'] ? 'align' . $block['align'] : '';
         while ( have_rows('carousel') ) : the_row(); ?>
     
         <div class="single-slide">
-            <a target="_blank" href="<?php the_sub_field('link'); ?>"><img src="<?php the_sub_field('image'); ?>" alt="<?php the_sub_field('title'); ?>">
+            <a target="_blank" href="<?php echo (wp_is_mobile()) ? get_sub_field('mobile_link') : get_sub_field('link'); ?>"><img src="<?php the_sub_field('image'); ?>" alt="<?php the_sub_field('title'); ?>">
             <h4><?php the_sub_field('title'); ?></h4>
-            <p><?php the_sub_field('subtitle'); ?></p>
+            <p class="subtitle"><?php the_sub_field('subtitle'); ?></p>
             </a>
         </div>
 
@@ -39,6 +42,7 @@ $align_class = $block['align'] ? 'align' . $block['align'] : '';
 
     ?>
     </div>
+</div>
 </div>
 				
 				
