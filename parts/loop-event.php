40d55969ca68e6b6c46bdd6d8114cfdbc53db177
giovanni puccio <giovanni.puccio@gmail.com>
2022-02-07 12:25:22 +0100

    add date to event

--- parts/loop-event.php
+++ parts/loop-event.php
@@ -7,7 +7,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class('content'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 						
 	<header class="article-header">	
-		<h1 class="entry-title single-title" itemprop="headline"><?php the_field('event_date'); ?> | <?php the_title(); ?></h1>
+		<h1 class="entry-title single-title" itemprop="headline"><?php echo DateTime::createFromFormat('Ymd', get_field('event_date'))->format('M jS, Y'); ; ?> | <?php the_title(); ?></h1>
     </header> <!-- end article header -->
 					
     <section class="entry-content" itemprop="text">
