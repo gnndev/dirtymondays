f8c41de773c1f7271440968e03625bacf8feb746
giovanni puccio <giovanni.puccio@gmail.com>
2022-09-01 10:51:07 +0200

    fix margins

--- parts/blocks/content-image-btn.php
+++ parts/blocks/content-image-btn.php
@@ -11,6 +11,6 @@ $align_class = $block['align'] ? 'align' . $block['align'] : '';
     <div style="position:relative">
     <a href="https://dirtymondays.com/shop/">
     <img style="width:100%" src="https://dirtymondays.com/wp-content/uploads/2022/08/dmo_home_2207_moodboard_V6C.jpg" alt=""></a>
-    <a style="position:absolute;top:70%;left:50%;font-size:22px;transform: translateX(-50%);" class="submit-button button" href="https://dirtymondays.com/shop/">SHOP NOW</a>
+    <a class="submit-button button img-btn-submit" href="https://dirtymondays.com/shop/">SHOP NOW</a>
     </div>
  </div>
