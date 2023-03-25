aa1e1599a0cc9f5e055e643f9171f39033381b8a
gnndev <62555442+gnndev@users.noreply.github.com>
2023-03-25 09:23:39 +0100

    quantity in checkout

--- functions/woocommerce.php
+++ functions/woocommerce.php
@@ -247,7 +247,7 @@ add_action( 'wp_footer', 'bbloomer_add_cart_quantity_plus_minus' );
   
 function bbloomer_add_cart_quantity_plus_minus() {
  
-   if ( ! is_product() && ! is_cart() ) return;
+   if ( ! is_product() && ! is_cart() && ! is_checkout() ) return;
     
    wc_enqueue_js( "   
            
@@ -276,4 +276,51 @@ function bbloomer_add_cart_quantity_plus_minus() {
       });
         
    " );
+}
+
+
+/**
+ * @snippet       Item Quantity Inputs @ WooCommerce Checkout
+ * @how-to        Get CustomizeWoo.com FREE
+ * @author        Rodolfo Melogli
+ * @compatible    WooCommerce 6
+ * @donate $9     https://businessbloomer.com/bloomer-armada/
+ */
+ 
+// ----------------------------
+// Add Quantity Input Beside Product Name
+   
+add_filter( 'woocommerce_checkout_cart_item_quantity', 'bbloomer_checkout_item_quantity_input', 9999, 3 );
+  
+function bbloomer_checkout_item_quantity_input( $product_quantity, $cart_item, $cart_item_key ) {
+   $product = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
+   $product_id = apply_filters( 'woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key );
+   if ( ! $product->is_sold_individually() ) {
+      $product_quantity = woocommerce_quantity_input( array(
+         'input_name'  => 'shipping_method_qty_' . $product_id,
+         'input_value' => $cart_item['quantity'],
+         'max_value'   => $product->get_max_purchase_quantity(),
+         'min_value'   => '0',
+      ), $product, false );
+      $product_quantity .= '<input type="hidden" name="product_key_' . $product_id . '" value="' . $cart_item_key . '">';
+   }
+   return $product_quantity;
+}
+ 
+// ----------------------------
+// Detect Quantity Change and Recalculate Totals
+ 
+add_action( 'woocommerce_checkout_update_order_review', 'bbloomer_update_item_quantity_checkout' );
+ 
+function bbloomer_update_item_quantity_checkout( $post_data ) {
+   parse_str( $post_data, $post_data_array );
+   $updated_qty = false;
+   foreach ( $post_data_array as $key => $value ) {   
+      if ( substr( $key, 0, 20 ) === 'shipping_method_qty_' ) {         
+         $id = substr( $key, 20 );   
+         WC()->cart->set_quantity( $post_data_array['product_key_' . $id], $post_data_array[$key], false );
+         $updated_qty = true;
+      }      
+   }   
+   if ( $updated_qty ) WC()->cart->calculate_totals();
 }
